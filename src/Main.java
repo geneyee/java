56cd2c4f2395f36025c12b077c01e6470756727d
geneyee <geneyeey@gmail.com>
2023-06-02 13:45:17 +0900

    chap2 연습문제

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = 7 + 7 + "";
+        String str2 = "" + 7 + 7;
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}
