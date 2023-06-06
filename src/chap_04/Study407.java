10c3a05fed897877f455d9ec2d08cb45af09f432
geneyee <geneyeey@gmail.com>
2023-06-06 15:34:06 +0900

    연습문제 chap4 - 7번

---
+++ src/chap_04/Study407.java
@@ -0,0 +1,10 @@
+package chap_04;
+
+public class Study407 {
+    public static void main(String[] args) {
+        // Math.random()을 이용해서 1부터 6사이의 임의의 정수를 변수 value에 저장
+
+        int value = (int) (Math.random() * 6) + 1;
+        System.out.println("value : " + value);
+    }
+}
