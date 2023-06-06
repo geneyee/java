b45e154b2d0026dbd4806ae0ce17ebd31a5bef05
geneyee <geneyeey@gmail.com>
2023-06-06 15:38:49 +0900

    연습문제 chap4 - 12번

---
+++ src/chap_04/Study412.java
@@ -0,0 +1,13 @@
+package chap_04;
+
+public class Study412 {
+    public static void main(String[] args) {
+        // 구구단
+        for (int i = 2; i <= 7; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(i+"x"+j+"="+i*j);
+            }
+            System.out.println();
+        }
+    }
+}
