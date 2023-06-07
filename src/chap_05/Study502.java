9edd9540ab47c390006576ff3615877a0b55bd0b
geneyee <geneyeey@gmail.com>
2023-06-07 09:32:38 +0900

    연습문제 chap5 - 2번

---
+++ src/chap_05/Study502.java
@@ -0,0 +1,15 @@
+package chap_05;
+
+public class Study502 {
+    public static void main(String[] args) {
+        // arr[3].lenght?
+        int[][] arr = {
+                {5, 5, 5, 5, 5},
+                {10, 10, 10},
+                {20, 20, 20, 20, 20},
+                {30, 30}
+        };
+        System.out.println(arr[3].length); // 2
+
+    }
+}
