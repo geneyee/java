1e0b8b26eff3231061a9c0cfd6e3c120c3fdb6fd
geneyee <geneyeey@gmail.com>
2023-06-07 09:28:52 +0900

    연습문제 chap5 -1번

---
+++ src/chap_05/Study501.java
@@ -0,0 +1,38 @@
+package chap_05;
+
+public class Study501 {
+    public static void main(String[] args) {
+        // 배열 선언 초기화.
+        // 잘못된 것 고르기
+
+        /*
+            a. int[] arr[];
+            -> int[] arr; int arr[];
+         */
+
+        /*
+            b. int[] arr = {1, 2, 3};
+         */
+
+        /*
+            c. int[] arr = new int[5];
+         */
+
+        /*
+            d. int[] arr = new int[5]{1,2,3,4,5};
+            -> int[] arr = new int[5];
+               int[] arr = {1,2,3,4,5};
+         */
+        /*
+            e. int arr[5];
+            int[] arr = new int[5];
+         */
+
+        /*
+            f. int[] arr[] = new int[3][];
+            -> int[] arr = new int[3];
+         */
+
+
+    }
+}
