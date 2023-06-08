54f70113cf52004f466ccf8474fce061687a91b4
geneyee <geneyeey@gmail.com>
2023-06-08 13:30:38 +0900

    chap5 - 8번 내 풀이

--- src/chap_05/Study508.java
+++ src/chap_05/Study508.java
@@ -8,15 +8,27 @@ public class Study508 {
         int[] counter = new int[4];
 
         for (int i = 0; i < answer.length; i++) {
-            if (answer[i] == answer[0]) {
-                int count =0;
-                count++;
-                counter[i]=count;
+            switch (answer[i]){
+                case 1 :
+                    counter[0]++;
+                    break;
+                case 2 :
+                    counter[1]++;
+                    break;
+                case 3 :
+                    counter[2]++;
+                    break;
+                case 4:
+                    counter[3]++;
+                    break;
             }
         }
 
         for (int i = 0; i < counter.length; i++) {
-
+            System.out.print(counter[i]);
+            for (int j = 0; j < counter[i]; j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
