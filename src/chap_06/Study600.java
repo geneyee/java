3d6a006f22bb7df88eba377b9e879ee53e65eb22
geneyee <geneyeey@gmail.com>
2023-06-12 12:35:28 +0900

    chap6 메서드 예제 연습(max, min)

--- src/chap_06/Study600.java
+++ src/chap_06/Study600.java
@@ -2,7 +2,7 @@ package chap_06;
 
 public class Study600 {
     public static void main(String[] args) {
-        // 두 값을 받아서 둘 중에 큰 값을 반환하는 메서드
+        // 두 값을 받아서 둘 중에 큰 값을 반환하는 메서드(max)
         System.out.println(max(3,5));
 
         int result = max(6, 5);
