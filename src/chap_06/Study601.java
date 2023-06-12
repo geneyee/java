0e95bdf1dff83514502e4e13509802e00ec5c16d
geneyee <geneyeey@gmail.com>
2023-06-12 13:22:59 +0900

    연습문제 chap6 - 1번

---
+++ src/chap_06/Study601.java
@@ -0,0 +1,13 @@
+package chap_06;
+
+public class Study601 {
+        // 다음과 같은 멤버변수를 갖는 SutaCard 클래스 정의
+        // int num -> 카드의 숫자. (1~10사이의 정수)
+        // boolean isKwang -> 광이면 true, 아니면 false
+}
+
+class SutaCard {
+    private int num; //0
+    private boolean isKwang; //false
+
+}
