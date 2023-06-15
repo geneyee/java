f0648cec6d9b60ebe15c5755526f7a0ef173f8d7
geneyee <geneyeey@gmail.com>
2023-06-15 18:07:34 +0900

    연습문제 chap7 - 8번

---
+++ src/chap_07/Study708.java
@@ -0,0 +1,11 @@
+package chap_07;
+
+public class Study708 {
+    // Q. 다음 중 접근제어자를 접근범위가 넓은 것에서 좁은 것의 순으로 바르게 나열한 것은?
+    // 1. public - protected - (default) - private o
+
+    // public 모든 클래스에서
+    // protected 같은 패키지 내에서 단, 다른 패키지일 경우 자식 클래스에서는 가능
+    // default 같은 패키지
+    // private 해당 클래스
+ }
