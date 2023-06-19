803392fc2af1ddd755f156a69a2d2698c282e9ce
geneyee <geneyeey@gmail.com>
2023-06-19 17:56:38 +0900

    연다형성 7 - 15번

---
+++ src/chap_07/Study715.java
@@ -0,0 +1,27 @@
+package chap_07;
+
+public class Study715 {
+    public static void main(String[] args) {
+
+        Unit u = new GroundUnit();
+        Tank t = new Tank();
+        AirCraft ac = new AirCraft();
+
+        u = (Unit) ac;
+        u = ac;
+        GroundUnit gu = (GroundUnit) u;
+        AirUnit au = ac;
+        //    t = (Tank) u;
+        GroundUnit gu1 = t;
+    }
+}
+
+class Unit {}
+
+class AirUnit extends Unit {}
+
+class GroundUnit extends Unit {}
+
+class Tank extends GroundUnit {}
+
+class AirCraft extends AirUnit {}
