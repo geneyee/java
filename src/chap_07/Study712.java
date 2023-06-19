21a1dc3d4c529409a5b4c930703a85c6c7ec94fe
geneyee <geneyeey@gmail.com>
2023-06-19 11:26:53 +0900

    연습문제 7-12번(접근 제어자)

---
+++ src/chap_07/Study712.java
@@ -0,0 +1,15 @@
+package chap_07;
+
+public class Study712 {
+    public static void main(String[] args) {
+        // Q. 접근 제어자의 대한 설명으로 옳지 않은 것은?
+        // 1. public은 접근제한이 전혀 없는 접근 제어자이다. o
+        // 2. (default)가 붙으면 같은 패키지 내에서만 접근이 가능하다. o
+        // 3. 지역변수에도 접근 제어자를 사용할 수 있다. x
+        // 4. protected가 붙으면 같은 패키지 내에서도 접근이 가능하다. o
+        // 5. protected가 붙으면 다른 패키지의 자손 클래스에서 접근이 가능하다. o
+
+        // 접근 제어자 : 클래스, 클래스 멤버
+
+    }
+}
