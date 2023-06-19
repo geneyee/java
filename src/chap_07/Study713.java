47e5c272663b206d31d9803203376b8bd3128808
geneyee <geneyeey@gmail.com>
2023-06-19 11:42:33 +0900

    연습문제 7-13번

---
+++ src/chap_07/Study713.java
@@ -0,0 +1,10 @@
+package chap_07;
+
+public class Study713 {
+    public static void main(String[] args) {
+       // Math.random();
+
+        // Q. Math클래스의 생성자는 접근 제어자가 private이다. 그 이유는 무엇일까?
+        // 다른 클래스에서 Math 클래스의 인스턴스를 생성할 수 없도록 하기 위해서
+    }
+}
