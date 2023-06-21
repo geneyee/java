b49fd954234df9183feeb35a7b29eccdbd91a190
geneyee <geneyeey@gmail.com>
2023-06-21 17:19:53 +0900

    BufferedWriter, BufferedReader, StringTokenizer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,52 @@
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        String str = 7 + 7 + "";
-        String str2 = "" + 7 + 7;
-        System.out.println(str);
-        System.out.println(str2);
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        bw.write("hello");
+//        bw.flush();
+//        bw.close();
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        int[] arrays = new int[n];
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int sum = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            arrays[i] = Integer.parseInt(st.nextToken());
+//            sum += arrays[i];
+//        }
+//        System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+//        double A = sc.nextDouble();
+//        double B = sc.nextDouble();
+//        System.out.println(A/B);
+
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//
+//        System.out.println(A + B);
+//        System.out.println(A - B);
+//        System.out.println(A * B);
+//        System.out.println(A / B);
+//        System.out.println(A % B);
+
+        String str = sc.nextLine();
+
+        System.out.println(str+"??!");
+
+
+
+
+
+
     }
 }
