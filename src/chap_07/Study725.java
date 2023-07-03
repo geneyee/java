f6252d2ee425476c274d0f59d6c0086af341ddaf
geneyee <geneyeey@gmail.com>
2023-07-03 17:30:00 +0900

    7 - 25번

---
+++ src/chap_07/Study725.java
@@ -0,0 +1,13 @@
+package chap_07;
+
+public class Study725 {
+    public static void main(String[] args) {
+//        Outer.Inner inner = new Outer.Inner();
+//        System.out.println(inner.iv);
+    }
+}
+class Outer {
+    class Inner {
+        int iv = 100;
+    }
+}
